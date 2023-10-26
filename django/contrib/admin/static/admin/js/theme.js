05e8cca41813c4ce6aef715924629fce77bb834d
Scott Vella <101610785+thebugcollector@users.noreply.github.com>
2023-10-26 15:33:39 +1100

    Fixed #34670 -- Django Admin light/dark theme flicker
    Moved the setTheme and initTheme functions and the initTheme
    function call outside of the event page load.

--- django/contrib/admin/static/admin/js/theme.js
+++ django/contrib/admin/static/admin/js/theme.js
@@ -1,15 +1,23 @@
 'use strict';
 {
-    window.addEventListener('load', function(e) {
-
-        function setTheme(mode) {
-            if (mode !== "light" && mode !== "dark" && mode !== "auto") {
-                console.error(`Got invalid theme mode: ${mode}. Resetting to auto.`);
-                mode = "auto";
-            }
-            document.documentElement.dataset.theme = mode;
-            localStorage.setItem("theme", mode);
+    function setTheme(mode) {
+        if (mode !== "light" && mode !== "dark" && mode !== "auto") {
+            console.error(`Got invalid theme mode: ${mode}. Resetting to auto.`);
+            mode = "auto";
         }
+        document.documentElement.dataset.theme = mode;
+        localStorage.setItem("theme", mode);
+    }
+
+    function initTheme() {
+        // set theme defined in localStorage if there is one, or fallback to auto mode
+        const currentTheme = localStorage.getItem("theme") || "auto";
+        setTheme(currentTheme);
+    }
+
+    initTheme();
+
+    window.addEventListener('load', function(e) {
 
         function cycleTheme() {
             const currentTheme = localStorage.getItem("theme") || "auto";
@@ -36,19 +44,12 @@
             }
         }
 
-        function initTheme() {
-            // set theme defined in localStorage if there is one, or fallback to auto mode
-            const currentTheme = localStorage.getItem("theme");
-            currentTheme ? setTheme(currentTheme) : setTheme("auto");
-        }
-
         function setupTheme() {
             // Attach event handlers for toggling themes
             const buttons = document.getElementsByClassName("theme-toggle");
             Array.from(buttons).forEach((btn) => {
                 btn.addEventListener("click", cycleTheme);
             });
-            initTheme();
         }
 
         setupTheme();
